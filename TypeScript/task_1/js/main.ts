fd9dbdba00a99e45f5b00715f603f258d7be322e
victormc13 <viktorsb14@gmail.com>
2024-03-06 13:24:51 -0500

    update: Removes example code to test the 'Directors' interface that extends from 'Teacher'

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -12,11 +12,3 @@ interface Directors extends Teacher {
   numberOfReports: number
 }
 
-const director1: Directors = {
-  firstName: 'John',
-  lastName: 'Doe',
-  location: 'London',
-  fullTimeEmployee: true,
-  numberOfReports: 17,
-};
-console.log(director1);
