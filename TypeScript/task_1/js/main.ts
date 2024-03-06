b50d43e8cbaf0f0d87935ec7ffc78fff7cda05d1
victormc13 <viktorsb14@gmail.com>
2024-03-06 13:41:15 -0500

    update: Writes a function 'printTeacher' and the 'printTeacherFunction' interface

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -12,3 +12,11 @@ interface Directors extends Teacher {
   numberOfReports: number
 }
 
+function printTeacher(firstName: string, lastName: string): string {
+  const firstLetterName = firstName.charAt(0).toUpperCase();
+  return `${firstLetterName}. ${lastName}`;
+}
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
