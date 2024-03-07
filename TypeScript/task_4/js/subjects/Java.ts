76a708925af3778e077d39bde7358a93e32a9e28
victormc13 <viktorsb14@gmail.com>
2024-03-07 01:36:37 -0800

    new: Creates the 'Java' class in the same namespace('Subjects')

---
+++ TypeScript/task_4/js/subjects/Java.ts
@@ -0,0 +1,19 @@
+namespace Subjects {
+  export interface Teacher extends Subjects.Teacher {
+    experienceTeachingJava?: number;
+  }
+
+  export class Java extends Subjects.Subject {
+    getRequirements(): string {
+      return "Here is the list of requirements for Java";
+    }
+
+    getAvailableTeacher(): string {
+      if (this.teacher && this.teacher.experienceTeachingJava) {
+        return `Available Teacher: ${super.this.teacher.firstName}`;
+      } else {
+        return "No available teacher";
+      }
+    }
+  }
+}
