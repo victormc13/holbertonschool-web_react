7eb643a5e003279fdc54d83cf647f82383e1fd21
victormc13 <viktorsb14@gmail.com>
2024-03-07 01:35:06 -0800

    new: Uses declration merging to add new optional attribute to the 'Teacher' interface

---
+++ TypeScript/task_4/js/subjects/Cpp.ts
@@ -0,0 +1,19 @@
+namespace Subjects {
+  export interface Teacher extends Subject.Teacher {
+    experienceTeachingC?: number;
+  }
+
+  export class Cpp extends Subjects.Subject {
+    getRequirements(): string {
+      return "Here is the list of requirements for Cpp";
+    }
+
+    getAvailableTeacher(): string {
+      if (this.teacher && this.teacher.experienceTeachingC) {
+        return `Available Teacher: ${super.this.teacher.firstName}`;
+      } else {
+        return "No availalbe teacher";
+      }
+    }
+  }
+}
