c317b1427caa594dbc80ee20299ae8cb727dbffd
victormc13 <viktorsb14@gmail.com>
2024-03-07 01:36:08 -0800

    new: Creates the 'React' class in the same namespace('Subjects')

---
+++ TypeScript/task_4/js/subjects/React.ts
@@ -0,0 +1,19 @@
+namespace Subjects {
+  export interface Teacher extends Subjects.Teacher {
+    experienceTeachingReact?: number;
+  }
+
+  export class React extends Subjects.Subject {
+    getRequirements(): string {
+      return "Here is the list of requirements for React";
+    }
+
+    getAvailableTeacher(): string {
+      if (this.teacher && this.teacher.experienceTeachingReact) {
+        return `Available Teacher: ${super.this.teacher.firstName}`;
+      } else {
+        return "No available teacher for React";
+      }
+    }
+  }
+}
