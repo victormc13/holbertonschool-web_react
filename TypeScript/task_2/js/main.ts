0153ac9647c9bfae6d00f911eb70d20f0064dd62
victormc13 <viktorsb14@gmail.com>
2024-03-06 20:29:40 -0800

    update: Creates 'isDirector' and 'executeWork' functions that accepts 'employee' as an argument and calls it's own methods

--- TypeScript/task_2/js/main.ts
+++ TypeScript/task_2/js/main.ts
@@ -47,3 +47,15 @@ function createEmployee(salary: number | string): Director | Teacher {
     }
 }
 
+function isDirector(employee: Director | Teacher): employee is Director {
+  return (employee as Director).workDirectorTasks !== undefined;
+}
+
+function executeWork(employee: Director | Teacher): string {
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks();
+  } else {
+    return employee.workTeacherTasks();
+    }
+}
+
