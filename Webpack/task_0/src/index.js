f4ec70ad28ce497d8d4a416294910eeb11394393
victormc13 <viktorsb14@gmail.com>
2024-03-08 18:34:56 -0800

    update: Updates the code appending each 'p' tag individually

--- Webpack/task_0/src/index.js
+++ Webpack/task_0/src/index.js
@@ -1,7 +1,5 @@
 import $ from 'jquery';
 
-$('body').append([
-  $('<p>').text('Holberton Dashboard'),
-  $('<p>').text('Dashboard data for the students'),
-  $('<p>').text('Copyright - Holberton School'),
-]);
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<p>Copyright - Holberton School</p>');
