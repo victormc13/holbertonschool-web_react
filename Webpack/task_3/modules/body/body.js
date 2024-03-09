a6f90b8a1270abf1164c35041f63efb25d055321
victormc13 <viktorsb14@gmail.com>
2024-03-09 10:59:56 -0800

    new: Imports jquery and lodash to render button and counter

---
+++ Webpack/task_3/modules/body/body.js
@@ -0,0 +1,16 @@
+import $ from 'jquery';
+import _ from 'lodash';
+
+let count = 0;
+function updateCounter() {
+  count++;
+  $('#count').text(`${count} clicks on the button`)
+}
+
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button type="button">Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+
+const debouncedUpdate = _.debounce(updateCounter, 500)
+
+$('button').on('click', debouncedUpdate)
