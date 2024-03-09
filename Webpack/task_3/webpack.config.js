c1a20ca4bcc60e8956b680bb179d01b36ef45e3a
victormc13 <viktorsb14@gmail.com>
2024-03-09 10:34:19 -0800

    new: Sets the mode to development, and config devServer to run in port: 8564 for the public folder

---
+++ Webpack/task_3/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './js/dashboard_main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        use: [
+          'file-loader', // Use file-loader for basic image handling
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65,
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  devServer: {
+    contentBase: './public',
+    open: true,
+    port: 8564,
+  },
+};
