b7b43f01c37eca8435cbf6eb51290b119764f227
victormc13 <viktorsb14@gmail.com>
2024-03-09 00:54:45 -0800

    update: Imports styles and image for the holberton-logo.jpg image

--- Webpack/task_2/js/dashboard_main.js
+++ Webpack/task_2/js/dashboard_main.js
@@ -1,5 +1,7 @@
 import $ from 'jquery';
 import _ from 'lodash';
+import '../css/main.css';
+import holbertonImg from '../assets/holberton-logo.jpg';
 
 let count = 0;
 function updateCounter() {
@@ -7,7 +9,9 @@ function updateCounter() {
   $('#count').text(`${count} clicks on the button`)
 }
 
-$('body').append('<a id="logo" href="index.html"></a>')
+const image = `<img src="${holbertonImg}" alt="holberton-log" width="200" height="200">`
+
+$('body').append(`<a id="logo" href="index.html">${image}</a>`)
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button type="button">Click here to get started</button>');
