e8ef4e371bf39457ff39bf309754ceb99940bb09
victormc13 <viktorsb14@gmail.com>
2024-03-09 01:07:11 -0800

    update: Removes img tag inside a tag of the image that will be handled by the 'main.css' file

--- Webpack/task_2/js/dashboard_main.js
+++ Webpack/task_2/js/dashboard_main.js
@@ -1,7 +1,6 @@
 import $ from 'jquery';
 import _ from 'lodash';
 import '../css/main.css';
-import holbertonImg from '../assets/holberton-logo.jpg';
 
 let count = 0;
 function updateCounter() {
@@ -9,9 +8,8 @@ function updateCounter() {
   $('#count').text(`${count} clicks on the button`)
 }
 
-const image = `<img src="${holbertonImg}" alt="holberton-log" width="200" height="200">`
 
-$('body').append(`<a id="logo" href="index.html">${image}</a>`)
+$('body').append(`<a id="logo" href="index.html"></a>`)
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button type="button">Click here to get started</button>');
