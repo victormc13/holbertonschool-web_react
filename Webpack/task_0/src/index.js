4ee2241487ce2be9e64fd1b383ca41a8272455e9
victormc13 <viktorsb14@gmail.com>
2024-03-08 18:24:08 -0800

    update: Refactor code to append using an array of p tag with text content

--- Webpack/task_0/src/index.js
+++ Webpack/task_0/src/index.js
@@ -1,15 +1,9 @@
 import $ from 'jquery';
 
-$(document).ready(() => {
-  const paragraphsContent = [
-    'Holberton Dashboard',
-    'Dashboard data for the students',
-    'Copyright - Holberton School'
-  ];
-
-  $(paragrapahsContent).each((index, value) => {
-    const newParagraph = $('<p></p>').text.value;
-
-    $('body').append(newParagraph);
-  })
-})
+$(document).ready(function() {
+  $('body').append([
+    $('<p>').text('Holberton Dashboard'),
+    $('<p>').text('Dashboard data for the students'),
+    $('<p>').text('Copyright - Holberton School'),
+  ]);
+});
