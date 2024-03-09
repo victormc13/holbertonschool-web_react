97b33afede4aee3fc9520c3b85e8cd46d16d20d0
victormc13 <viktorsb14@gmail.com>
2024-03-08 18:29:54 -0800

    update: Refactor code removing 'ready' method instead it appends 'p' tags immediately

--- Webpack/task_0/src/index.js
+++ Webpack/task_0/src/index.js
@@ -1,9 +1,7 @@
 import $ from 'jquery';
 
-$(document).ready(function() {
-  $('body').append([
-    $('<p>').text('Holberton Dashboard'),
-    $('<p>').text('Dashboard data for the students'),
-    $('<p>').text('Copyright - Holberton School'),
-  ]);
-});
+$('body').append([
+  $('<p>').text('Holberton Dashboard'),
+  $('<p>').text('Dashboard data for the students'),
+  $('<p>').text('Copyright - Holberton School'),
+]);
