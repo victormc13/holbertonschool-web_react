61965d5991284b273562bef74a52ed7af30adeb8
victormc13 <viktorsb14@gmail.com>
2024-03-12 15:45:49 -0500

    update: Creates a function named 'getLatestNotification' that returns a string that use an html tag

--- react_intro/task_2/dashboard/src/utils.js
+++ react_intro/task_2/dashboard/src/utils.js
@@ -5,3 +5,7 @@ export function getFullYear() {
 export function getFooterCopy(isIndex) {
   return isIndex ? 'Holberton School' : 'Holberton School main dashboard';
 }
+
+export function getLatestNotification() {
+  return '<strong>Urgent requirement</strong> - complete by EOD';
+}
