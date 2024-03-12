21f5ca9e1563fb2d0b27133dfd8c223054f7f833
victormc13 <viktorsb14@gmail.com>
2024-03-11 23:59:02 -0500

    update: Updates to use functions created from utils.js

---
+++ react_intro/task_1/dashboard/src/App.js
@@ -0,0 +1,22 @@
+import hlogo from './holberton-logo.jpg';
+import './App.css';
+import { getFullYear, getFooterCopy } from './utils';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={hlogo} className="App-logo" alt="holberton logo" />
+        <h1>School dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+      <div className="App-footer">
+        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
