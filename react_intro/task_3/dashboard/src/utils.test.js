09521ae1a80caab9efc9ec6d27156e44021d2690
victormc13 <viktorsb14@gmail.com>
2024-03-13 01:14:21 -0700

    update: Place in diferent test cases for the 'getFooterCopy' function depending of the isIndex value

--- react_intro/task_3/dashboard/src/utils.test.js
+++ react_intro/task_3/dashboard/src/utils.test.js
@@ -1,18 +1,25 @@
 import { getFullYear, getFooterCopy, getLatestNotification } from './utils'
 
-test('getFullYear returns the current year', () => {
-  const expectedYear = new Date().getFullYear();
-  const actualYear = getFullYear();
 
-  expect(actualYear).toBe(expectedYear);
-})
+describe('Test the functions from utils.js file', () => {
+  it('getFullYear returns the current year', () => {
+    const expectedYear = new Date().getFullYear();
+    const actualYear = getFullYear();
+  
+    expect(actualYear).toBe(expectedYear);
+  })
+  
+  it('getFooterCopy returns the correct string based on isIndex being "true"', () => {
+    expect(getFooterCopy(true)).toBe('Holberton School');
+  })
 
-test('getFooterCopy returns the correct string based on isIndex', () => {
-  expect(getFooterCopy(true)).toBe('Holberton School');
-  expect(getFooterCopy(false)).toBe('Holberton School main dashboard');
-})
-
-test('getLatestNotification returns the expected string', () => {
-  const expectedString = '<strong>Urgent requirement</strong> - complete by EOD';
-  expect(getLatestNotification()).toBe(expectedString);
+  it('getFooterCopy returns the correct string based on isIndex being "false"', () => {
+    expect(getFooterCopy(false)).toBe('Holberton School main dashboard');
+  })
+  
+  it('getLatestNotification returns the expected string', () => {
+    const expectedString = '<strong>Urgent requirement</strong> - complete by EOD';
+    expect(getLatestNotification()).toBe(expectedString);
+  })
+  
 })
