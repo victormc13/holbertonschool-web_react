968d5e90164b518b8d2bb962807b8287229d3753
victormc13 <viktorsb14@gmail.com>
2024-03-12 20:07:11 -0700

    update:  Adds two test cases for the 'getFooterCopy' and 'getLatestNotification' functions

--- react_intro/task_3/dashboard/src/utils.test.js
+++ react_intro/task_3/dashboard/src/utils.test.js
@@ -6,3 +6,13 @@ test('getFullYear returns the current year', () => {
 
   expect(actualYear).toBe(expectedYear);
 })
+
+test('getFooterCopy returns the correct string based on isIndex', () => {
+  expect(getFooterCopy(true)).toBe('Holberton School');
+  expect(getFooterCopy(false)).toBe('Holberton School main dashboard');
+})
+
+test('getLatestNotification returns the expected string', () => {
+  const expectedString = '<strong>Urgent requirement</strong> - complete by EOD';
+  expect(getLatestNotification()).toBe(expectedString);
+})
