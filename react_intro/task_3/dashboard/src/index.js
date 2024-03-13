5bc5ed0dfd0954c1a2cec0e409ae0debbc09e7d2
victormc13 <viktorsb14@gmail.com>
2024-03-13 00:32:05 -0700

    update: Install older version of React to can work with Enzyme and use older syntax for the rootElement

--- react_intro/task_3/dashboard/src/index.js
+++ react_intro/task_3/dashboard/src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Notifications } from './Notifications';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <div id='root-notifications'>
+    <div id="root-notifications">
       <Notifications />
     </div>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
