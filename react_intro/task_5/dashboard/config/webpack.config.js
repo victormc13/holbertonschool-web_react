905cdb8d9f98b0ad4c2e0eb97dceefd2997f45e7
victormc13 <viktorsb14@gmail.com>
2024-03-13 15:31:03 -0700

    new: Adds webpack config file with config for the bundle

---
+++ react_intro/task_5/dashboard/config/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+          },
+        ],
+      },
+    ],
+  },
+  devServer: {
+    contentBase: '../dist',
+    compress: true,
+    open: true,
+    hot: true,
+  },
+};
