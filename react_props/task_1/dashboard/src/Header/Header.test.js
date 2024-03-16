95e5e0a365185e9e8e5c55ae565951740835841a
victormc13 <viktorsb14@gmail.com>
2024-03-15 23:53:12 -0700

    update: Imports the Header component and writes two test cases to verify that component renders correctly with the img and h1 tags

---
+++ react_props/task_1/dashboard/src/Header/Header.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../Header/Header';
+import { shallow } from 'enzyme';
+
+describe('Header component test cases', () => {
+  it('renders the Header component without crashing', () => {
+    shallow(<Header />);
+  })
+
+  it('the component renders img and h1 tags', () => {
+    const wrapper = shallow(<Header />);
+    expect(wrapper.find('img')).toHaveLength(1);
+    expect(wrapper.find('h1')).toHaveLength(1);
+  })
+})
