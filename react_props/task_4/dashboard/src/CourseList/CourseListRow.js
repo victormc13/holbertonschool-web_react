496f91328b7266b7569e2dde00f422793edff98b
victormc13 <viktorsb14@gmail.com>
2024-03-17 17:37:23 -0700

    new: Creates CourseListRow component that receives 'isHeader', 'textFirstCell' and 'textSecondCell' props

---
+++ react_props/task_4/dashboard/src/CourseList/CourseListRow.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const CourseListRow = ({ isHeader=false, textFirstCell, textSecondCell=null }) => {
+  if (isHeader) {
+    if (textSecondCell === null) {
+      return (
+        <tr>
+          <th colSpan="2">{textFirstCell}</th>
+        </tr>
+      );
+    } else {
+      return (
+        <tr>
+          <th>{textFirstCell}</th>
+          <th>{textSecondCell}</th>
+        </tr>
+      );
+    }
+  } else {
+    return (
+      <tr>
+        <td>{textFirstCell}</td>
+        <td>{textSecondCell}</td>
+      </tr>
+    );
+  }
+};
+
+CourseListRow.propTypes = {
+  isHeader: PropTypes.bool,
+  textFirstCell: PropTypes.string.isRequired,
+  textSecondCell: PropTypes.string,
+};
+
+CourseListRow.defaultProps = {
+  isHeader: false,
+  textSecondCell: null,
+}
+
+export default CourseListRow
