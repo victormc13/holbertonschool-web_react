cdc64f1e88ad21fddb225beb750fe2acad0cbd1e
victormc13 <viktorsb14@gmail.com>
2024-03-22 19:23:57 -0500

    update: Modifies the li element to call 'markAsRead' function on click with the id of notification

---
+++ React_component/task_2/dashboard/src/Notifications/NotificationItem.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class NotificationItem extends React.Component {
+  render() {
+    const { type, html, value, markAsRead, id } = this.props;
+    return (
+      <li
+        data-notification-type={type}
+        dangerouslySetInnerHTML={html}
+        onClick={() => markAsRead(id)}
+      >
+        {value}
+      </li>
+    );
+  }
+}
+
+NotificationItem.propTypes = {
+  html: PropTypes.shape({ __html: PropTypes.string }),
+  type: PropTypes.string.isRequired,
+  value: PropTypes.string,
+  markAsRead: PropTypes.func,
+  id: PropTypes.number
+};
+
+NotificationItem.defaultProps = {
+  type: 'default',
+  markAsRead: () => {} 
+};
+
+export default NotificationItem;
