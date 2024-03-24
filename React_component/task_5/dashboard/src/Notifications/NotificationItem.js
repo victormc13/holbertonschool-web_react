2f1abcd2d7b27f57d41c081b51f7f4fbf50434a2
victormc13 <viktorsb14@gmail.com>
2024-03-24 17:27:39 -0500

    update: Updates the component converting to function component to can use the React.memo HOC

--- React_component/task_5/dashboard/src/Notifications/NotificationItem.js
+++ React_component/task_5/dashboard/src/Notifications/NotificationItem.js
@@ -1,42 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class NotificationItem extends React.Component {
-  shouldComponentUpdate(nextProps) {
-    return (
-      this.props.type !== nextProps.type ||
-      this.props.html.__html !== nextProps.html.__html ||
-      this.props.value !== nextProps.value ||
-      this.props.markAsRead !== nextProps.markAsRead ||
-      this.props.id !== nextProps.id
-    );
-  }
-
-  render() {
-    const { type, html, value, markAsRead, id } = this.props;
-    return (
-      <li
-        data-notification-type={type}
-        dangerouslySetInnerHTML={html}
-        onClick={() => markAsRead(id)}
-      >
-        {value}
-      </li>
-    );
-  }
-}
+const NotificationItem = React.memo(({ type, html, value, markAsRead, id }) => {
+  return (
+    <li
+      data-notification-type={type}
+      dangerouslySetInnerHTML={html}
+      onClick={() => markAsRead(id)}
+    >
+      {value}
+    </li>
+  );
+});
 
 NotificationItem.propTypes = {
   html: PropTypes.shape({ __html: PropTypes.string }),
   type: PropTypes.string.isRequired,
   value: PropTypes.string,
   markAsRead: PropTypes.func,
-  id: PropTypes.number
+  id: PropTypes.number,
 };
 
 NotificationItem.defaultProps = {
   type: 'default',
-  markAsRead: () => {} 
+  markAsRead: () => {},
 };
 
 export default NotificationItem;
