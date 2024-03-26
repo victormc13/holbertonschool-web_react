720ea0a00e18ca26a5945ece961c0a20d17b7b05
victormc13 <viktorsb14@gmail.com>
2024-03-26 01:07:30 -0500

    refactor: Modifies the component to use Aphrodite within the js file styling the Login section

---
+++ React_inline_styling/task_1/dashboard/src/Login/Login.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { StyleSheet, css } from 'aphrodite';
+
+
+const styles = StyleSheet.create({
+  loginInput: {
+    display: 'flex',
+    gap: '1rem',
+  },
+  loginInputButton: {
+    backgroundColor: 'white',
+    color: 'black',
+    border: '2px solid #e7e7e7;',
+    borderRadius: '8px',
+    cursor: 'pointer',
+  }
+})
+
+
+const Login = () => {
+  const handleLabelClick = (event) => {
+    const inputId = event.target.htmlFor;
+    document.getElementById(inputId).focus();
+  };
+
+  return (
+    <>
+      <p>Login to access the full dashboard</p>
+      <div className={css(styles.loginInput)}>
+        <label htmlFor="email" onClick={handleLabelClick}>
+          Email:
+        </label>
+        <input type="email" id="email" />
+        <label htmlFor="password" onClick={handleLabelClick}>
+          Password:
+        </label>
+        <input type="passwrod" id="password" />
+        <button type="button" className={css(styles.loginInputButton)}>OK</button>
+      </div>
+    </>
+  );
+};
+
+export default Login;
