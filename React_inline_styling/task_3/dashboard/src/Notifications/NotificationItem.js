0fae696108a43886dcbeb1d88f38b8fd7cc28322
victormc13 <viktorsb14@gmail.com>
2024-03-27 00:51:15 -0500

    update: Make the NotificationItem component responsive apply media queries with Aphrodite

---
+++ React_inline_styling/task_3/dashboard/src/Notifications/NotificationItem.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { StyleSheet, css } from 'aphrodite';
+
+const mediumScreenSize = {
+  padding: '10px 8px',
+  listStyle: 'none',
+  borderBottom: '1px solid black',
+  fontSize: '20px'
+};
+
+const styles = StyleSheet.create({
+  default: {
+    color: 'blue',
+    '@media (max-width: 900px)': mediumScreenSize,
+  },
+  urgent: {
+    color: 'red',
+    '@media (max-width: 900px)': mediumScreenSize,
+  },
+});
+
+const NotificationItem = React.memo(({ type, html, value, markAsRead, id }) => {
+  const color = css(styles[type]); // apply styles based on type value
+  return (
+    <li
+      className={color}
+      data-notification-type={type}
+      dangerouslySetInnerHTML={html}
+      onClick={() => markAsRead(id)}
+    >
+      {value}
+    </li>
+  );
+});
+
+NotificationItem.propTypes = {
+  html: PropTypes.shape({ __html: PropTypes.string }),
+  type: PropTypes.string.isRequired,
+  value: PropTypes.string,
+  markAsRead: PropTypes.func,
+  id: PropTypes.number,
+};
+
+NotificationItem.defaultProps = {
+  type: 'default',
+  markAsRead: () => {},
+};
+
+export default NotificationItem;
