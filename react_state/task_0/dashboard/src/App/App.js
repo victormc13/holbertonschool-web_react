119e8436cc6851e86c11a3b36e87ff37b4da1920
victormc13 <viktorsb14@gmail.com>
2024-03-30 19:10:27 -0500

    update: Creates a local state that stores the default value of 'displayDrawer' with handlers functions too

---
+++ react_state/task_0/dashboard/src/App/App.js
@@ -0,0 +1,117 @@
+import React from 'react';
+import Header from '../Header/Header';
+import Notifications from '../Notifications/Notifications';
+import Footer from '../Footer/Footer';
+import Login from '../Login/Login';
+import CourseList from '../CourseList/CourseList';
+import BodySection from '../BodySection/BodySection';
+import BodySectionWithMarginBottom from '../BodySection/BodySectionWithMarginBottom';
+import PropTypes from 'prop-types';
+import { getLatestNotification } from '../utils/utils';
+import { StyleSheet, css } from 'aphrodite';
+
+const styles = StyleSheet.create({
+  app: {
+    display: 'grid',
+    gridTemplateRows: 'auto 1fr auto',
+    minHeight: '100vh',
+    width: '100%',
+  },
+  body: {
+    borderTop: '4px solid #e0354b',
+    borderBottom: '4px solid #e0354b',
+    padding: '2rem',
+  },
+  footer: {
+    width: '100%',
+    textAlign: 'center',
+    fontStyle: 'italic',
+    fontSize: '1.25rem',
+  },
+});
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      listCourses: [
+        { id: 1, name: 'ES6', credit: 60 },
+        { id: 2, name: 'Webpack', credit: 20 },
+        { id: 3, name: 'React', credit: 40 },
+      ],
+      listNotifications: [
+        { id: 1, type: 'default', value: 'New course available' },
+        { id: 2, type: 'urgent', value: 'New resume available' },
+        { id: 3, type: 'urgent', html: { __html: getLatestNotification() } },
+      ],
+      displayDrawer: false
+    };
+  }
+
+  handleKeyDown = (event) => {
+    if (event.ctrlKey && event.key === 'h') {
+      alert('Logging you out');
+      this.props.logOut();
+    }
+  };
+
+  handleDisplayDrawer = () => {
+    this.setState({ displayDrawer: true })
+  }
+
+  handleHideDrawer = () => {
+    this.setState({ displayDrawer: false })
+  }
+
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  render() {
+    const { isLoggedIn } = this.props;
+
+    return (
+      <>
+        <Notifications listNotifications={this.state.listNotifications} displayDrawer={false}/>
+        <div className={css(styles.app)}>
+          <Header />
+          <div className={css(styles.body)}>
+            {isLoggedIn ? (
+              <BodySectionWithMarginBottom title="Course list">
+                <CourseList listCourses={this.state.listCourses} />
+              </BodySectionWithMarginBottom>
+            ) : (
+              <BodySectionWithMarginBottom title="Log in to continue">
+                <Login />
+              </BodySectionWithMarginBottom>
+            )}
+            {
+              <BodySection title="News from the School">
+                <p>More news soon...</p>
+              </BodySection>
+            }
+          </div>
+          <div className={css(styles.footer)}>
+            <Footer />
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+Notifications.propTypes = {
+  isLoggedIn: PropTypes.bool,
+  logOut: PropTypes.func,
+};
+
+Notifications.defaultProps = {
+  isLoggedIn: false,
+  logOut: () => {},
+};
+
+export default App;
